734d01fea16ef18dc05c95bfae0f2c720a94b18f
mjsj120 <63355903+mjsj120@users.noreply.github.com>
2020-07-06 18:36:04 +0900

    comment

--- KMU_likelion8th/likelion8th/blog/models.py
+++ KMU_likelion8th/likelion8th/blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
 
 class Blog(models.Model):
@@ -7,6 +7,7 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to="blog/", blank=True, null=True)
+    like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name = 'likers')
 
     def __str__(self):
         return self.title
@@ -14,5 +15,12 @@ class Blog(models.Model):
     def summary(self):
         return self.body[:100]
 
+class Comment(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name = 'comments')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null =True, blank = True, related_name = 'comments')
+    body = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"{self.author}님이 {self.blog}에 단 댓글"
 
